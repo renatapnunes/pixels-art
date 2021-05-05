fbe9228800d228cb857e0133165d9089f544ee84
Renata Nunes <renatanunes231@yahoo.com.br>
2021-05-05 16:11:40 -0300

    Requisitos 3, 4 e 5 - Concluidos

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// cria e adiciona paleta de cores
 let pallete = document.getElementById('color-palette');
 for (let i = 0; i < 4; i += 1) {
     let color = document.createElement('div');
@@ -6,8 +7,7 @@ for (let i = 0; i < 4; i += 1) {
     color.style.backgroundColor = arrayRadomColors[i];
     pallete.appendChild(color);
 }
-
-
+// gera rgb aleatorio
 function generateRandomColors () {
     let r = Math.ceil(Math.random()*254);
     let g = Math.ceil(Math.random()*254);
@@ -15,7 +15,7 @@ function generateRandomColors () {
     let rgb = ('rgb(' + r + ', ' + g + ', ' + b + ')');
     return rgb;
 }
-
+// gera array de 4 cores
 function generateArrayRandomColors () {
     let colorsArray = [];
     let colorRGB;
@@ -32,4 +32,21 @@ function generateArrayRandomColors () {
     }
     return colorsArray;
 }
+// quadro de pixels
+let board = document.getElementById('pixel-board');
+for (let i = 0; i < 5; i += 1) {
+    let pixelLine = document.createElement('div');
+    pixelLine.className = 'pixel-line';
+    board.appendChild(pixelLine);
+}
+let arrayPixelLine = document.getElementsByClassName('pixel-line');
+for (let i = 0; i < 5; i += 1) {
+    for (let j = 0; j < 5; j += 1) {
+        let pixel = document.createElement('div');
+        pixel.className = 'pixel';
+        pixel.style.backgroundColor = 'rgb(255, 255, 255)';
+        arrayPixelLine[i].appendChild(pixel);
+    }
+}
+
 
